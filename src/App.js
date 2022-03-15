3b85aa68012f296d3302faba5dab1372cd97851b
GleenPeltroche <gleenpeltroche@gmail.com>
2022-03-15 01:22:06 -0500

    gran inversion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';    
+import React from 'react';    
 import PersonCard from './PersonCard';
     
-class App extends Component {
-    render() {
-        return <div>
-          <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
-          <PersonCard firstName={"Jhon"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
-          <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
-          <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
-        </div>;
-    }
+const App = props => {
+    return (
+      <div>
+        <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+        <PersonCard firstName={"Jhon"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+        <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+        <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
+      </div>
+    ); 
 }
     
 export default App;
--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
     
-class PersonCard extends Component {
-    render() {
-        const { firstName, lastName, age, hairColor } = this.props;
-        return <div>
+const PersonCard = ({ firstName, lastName, age, hairColor }) => {
+    return (
+        <div>
             <h2>{lastName}, {firstName}</h2>
             <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
         </div>
-    }
+    )
 }
     
 export default PersonCard;
